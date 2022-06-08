0e02b434dca68fdaf6fe46c3d73992959c227d71
daehee <daehee.gatech@gmail.com>
2022-06-08 09:03:15 -0700

    add more px4 apps to libfuzzer init

--- docker/PX4-Autopilot/platforms/posix/src/px4/common/main_fuzztesting.cpp
+++ docker/PX4-Autopilot/platforms/posix/src/px4/common/main_fuzztesting.cpp
@@ -103,7 +103,94 @@ void initialize_fake_px4_once()
 	pxh.process_line("mc_pos_control start", true);
 	pxh.process_line("land_detector start multicopter", true);
 	pxh.process_line("logger start", true);
-	pxh.process_line("mavlink start -x -o 14540 -r 4000000", true);
+	
+    pxh.process_line("controllib_test start", true);
+    pxh.process_line("rc_tests start", true);
+    pxh.process_line("uorb_tests start", true);
+    pxh.process_line("wqueue_test start", true);
+    pxh.process_line("camera_trigger start", true);
+    pxh.process_line("gps start", true);
+    pxh.process_line("pwm_out_sim start", true);
+    pxh.process_line("rpm_simulator start", true);
+    //pxh.process_line("tone_alarm start", true);
+    pxh.process_line("airship_att_control start", true);
+    pxh.process_line("airspeed_selector start", true);
+    pxh.process_line("attitude_estimator_q start", true);
+    pxh.process_line("camera_feedback start", true);
+    //pxh.process_line("commander start", true);
+    pxh.process_line("commander_tests start", true);
+    pxh.process_line("control_allocator start", true);
+    //pxh.process_line("dataman start", true);
+    //pxh.process_line("ekf2 start", true);
+    pxh.process_line("send_event start", true);
+    pxh.process_line("flight_mode_manager start", true);
+    pxh.process_line("fw_att_control start", true);
+    pxh.process_line("fw_autotune_attitude_control start", true);
+    pxh.process_line("fw_pos_control_l1 start", true);
+    pxh.process_line("gimbal start", true);
+    pxh.process_line("gyro_calibration start", true);
+    pxh.process_line("gyro_fft start", true);
+    //pxh.process_line("land_detector start", true);
+    pxh.process_line("landing_target_estimator start", true);
+    //pxh.process_line("load_mon start", true);
+    pxh.process_line("local_position_estimator start", true);
+    //pxh.process_line("logger start", true);
+    pxh.process_line("mag_bias_estimator start", true);
+    pxh.process_line("manual_control start", true);
+    pxh.process_line("mavlink start", true);
+    pxh.process_line("mavlink_tests start", true);
+    //pxh.process_line("mc_att_control start", true);
+    pxh.process_line("mc_autotune_attitude_control start", true);
+    pxh.process_line("mc_hover_thrust_estimator start", true);
+    //pxh.process_line("mc_pos_control start", true);
+    pxh.process_line("mc_rate_control start", true);
+    //pxh.process_line("navigator start", true);
+    //pxh.process_line("rc_update start", true);
+    pxh.process_line("replay start", true);
+    pxh.process_line("rover_pos_control start", true);
+    //pxh.process_line("sensors start", true);
+    pxh.process_line("simulator start", true);
+    //pxh.process_line("battery_simulator start", true);
+    pxh.process_line("sensor_baro_sim start", true);
+    pxh.process_line("sensor_gps_sim start", true);
+    pxh.process_line("sensor_mag_sim start", true);
+    pxh.process_line("temperature_compensation start", true);
+    pxh.process_line("uuv_att_control start", true);
+    pxh.process_line("uuv_pos_control start", true);
+    pxh.process_line("vtol_att_control start", true);
+    pxh.process_line("actuator_test start", true);
+    pxh.process_line("dyn start", true);
+    pxh.process_line("failure start", true);
+    pxh.process_line("led_control start", true);
+    pxh.process_line("mixer start", true);
+    pxh.process_line("motor_test start", true);
+    pxh.process_line("param start", true);
+    pxh.process_line("perf start", true);
+    pxh.process_line("pwm start", true);
+    pxh.process_line("sd_bench start", true);
+    pxh.process_line("shutdown start", true);
+    pxh.process_line("system_time start", true);
+    pxh.process_line("tests start", true);
+    pxh.process_line("hrt_test start", true);
+    pxh.process_line("listener start", true);
+    pxh.process_line("tune_control start", true);
+    pxh.process_line("uorb start", true);
+    pxh.process_line("ver start", true);
+    pxh.process_line("work_queue start", true);
+    pxh.process_line("fake_gps start", true);
+    pxh.process_line("fake_imu start", true);
+    pxh.process_line("fake_magnetometer start", true);
+    pxh.process_line("ex_fixedwing_control start", true);
+    pxh.process_line("hello start", true);
+    pxh.process_line("px4_mavlink_debug start", true);
+    pxh.process_line("px4_simple_app start", true);
+    pxh.process_line("rover_steering_control start", true);
+    pxh.process_line("uuv_example_app start", true);
+    pxh.process_line("work_item_example start", true);
+    pxh.process_line("list_tasks start", true);
+    pxh.process_line("list_files start", true);
+        
+    pxh.process_line("mavlink start -x -o 14540 -r 4000000", true);
 	pxh.process_line("mavlink boot_complete", true);
 }
 
@@ -156,3 +243,5 @@ void send_mavlink(const uint8_t *data, const size_t size)
 
 	close(socket_fd);
 }
+
+
