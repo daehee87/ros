a5f03346080ecd020732c8c8016be31a74fc3b47
daehee <djang@sungshin.ac.kr>
2022-06-15 13:44:41 +0000

    add other sanitizers

---
+++ docker_tsan/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/PX4-Autopilot/build/px4_sitl_default/bin/px4 -workers=4 /out
+/bin/bash
+
---
+++ docker_tsan/run.sh
@@ -0,0 +1,12 @@
+ID="$(basename $PWD)"
+sudo docker build -t "ros-$ID" .
+sudo docker rm -f "ros-$ID-nick"
+# add --cap-add=SYS_PTRACE --security-opt seccomp=unconfined to debug nginx
+sudo docker run --cap-add=SYS_PTRACE \
+                --security-opt \
+                seccomp=unconfined \
+                -v $PWD/out:/out \
+                --name "ros-$ID-nick" \
+                -it \
+                "ros-$ID"
+
